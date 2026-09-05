6bfabeb4ef6062fb104bf81c075ee714465d16cb
Kosuru Harsha <lelaharsha@gmail.com>
2026-09-06 01:09:07 +0530

    fixed settings for email

--- project1/settings.py
+++ project1/settings.py
@@ -199,12 +199,14 @@ LOGIN_URL = "signup"
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
+EMAIL_TIMEOUT = 20
+EMAIL_USE_SSL = False
 EMAIL_USE_TLS = True
 
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 
-
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 # -------------------------------------------------------------------
 # Logging
 # -------------------------------------------------------------------
